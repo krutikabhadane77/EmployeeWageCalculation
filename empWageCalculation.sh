638cfab6855df07c1de0ac5612b57e8cecb12ae8
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-09-19 23:53:52 +0530

    Added in: UC4 Employee Wage Using Case Statement

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -2,14 +2,15 @@ isPartTime=1
 isFullTime=2
 empRatePerHr=20
 randomCheck=$((RANDOM%3))
-if [ $isFullTime -eq $randomCheck ];
+
+if [ $isFullTime -eq $randomCheck ]
+then	
+empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
 then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then 
-	empHrs=4
+empHrs=4;
 else
-	empHrs=0
+empHrs=0;
 fi
-	salary=$(($empHrs*$empRatePerHr))
-	echo "salary=$salary"
+salary=$(($empHrs*$empRatePerHr))
+echo "salary=$salary"
