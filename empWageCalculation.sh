26d5773d42edc748d3072a9f7c8a3f8218405ab5
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-09-20 01:05:03 +0530

    Added in: UC7 Calculating Wages till No of Working Days or Total Working Hours per month is Reached

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -6,26 +6,25 @@ numWorkingDays=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
+function getWorkingHours() {
+   case $1 in
+       $is_FullTime)
+         empHrs=8
+          ;;
+       $is_PartTime)
+         empHrs=4
+          ;;
+       *)
+         empHrs=0
+          ;;
+   esac
+ echo $empHrs;
+}
 
-while [[ $totalEmpHr -lt $MaxHrs_inMonth &&
+while [[ $totalempHrs -lt $MaxHrs_inMonth &&
          $totalWorkingDays -lt $numWorkingDays ]]
 do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3));
-	
-	case $empCheck in
-		$is_FullTime)
-			empHrs=8
-          		;;
-		$is_PartTime)
-         		empHrs=4
-          		;;
-       		*)
-         		empHrs=0
-          		;;
-   	esac
-totalEmpHr=$(($totalEmpHr + $empHrs)) 
+      ((totalWorkingDays++))
+      empHrs="$( getWorkingHours $((RANDOM%3)) )"
+      totalWorkHrs=$(($totalempHrs+$empHrs))
 done
-
-totalSalary=$(($totalEmpHr+$empRatePerHr));
-
